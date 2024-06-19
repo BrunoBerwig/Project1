10a1105ce4027dc486b8560645bf80833add5ff2
BrunoBerwig <contatobrunoberwig@gmail.com>
2024-06-19 19:18:34 -0300

    LocalStorage changes

--- js/login.js
+++ js/login.js
@@ -6,52 +6,60 @@ function registroLogin(){
     let user = document.getElementById("userName").value
     let pass = document.getElementById("pass").value
     let email = document.getElementById("email").value
+
     if (user === "" || pass === "" || email === "") {
         document.getElementById("userName").value = ""
         document.getElementById("pass").value = ""
         document.getElementById("email").value = ""
         document.getElementById("erroLogin").style.display = "block"
-        return
+        return;
     }
+
     if (email.indexOf("@gmail.com") === -1) {
         document.getElementById("erroLogin").style.display = "block"
         return
-        
     }
-    alert("Cadastrado")
+    let users = JSON.parse(localStorage.getItem("users")) || []
+
+
+    users.push({ user: user, pass: pass, email: email })
+
+    localStorage.setItem("users", JSON.stringify(users))
+
+    alert("Cadastrado");
     document.getElementById("registrado").style.display = "block"
     window.location.href = "index.html"
-    localStorage.setItem("email", email)
-    localStorage.setItem("user", user)
-    localStorage.setItem("senha", pass)
 }
+
 function validaLogin(){
     let user = document.getElementById("userName2").value
     let pass = document.getElementById("pass2").value
-    let usuLS = localStorage.getItem("user")
-    let passLS = localStorage.getItem("senha")
 
-    if(user == usuLS && pass == passLS){
+    let users = JSON.parse(localStorage.getItem("users")) || []
+
+    let foundUser = users.find(u => u.user === user && u.pass === pass)
+
+    if (foundUser) {
         document.getElementById("logado").style.display = "block"
         window.location.href = "paginainicial.html"
-    }else{
+    } else {
         document.getElementById("erroLogin").style.display = "block"
         document.getElementById("userName2").value = ""
-        document.getElementById("pass2").value = "" 
-       
-  
-    } 
+        document.getElementById("pass2").value = ""
+    }
 }
-function recuperarSenha(){
+function recuperarSenha() {
     let email = document.getElementById("email").value
-    let emailLS = localStorage.getItem("email")
-
-    if(email == emailLS){
-    alert(`Um email de recuperação de senha foi enviado para ${email}`)
-    window.location.href = "index.html"
-    }else{
-        document.getElementById("erroLogin").style.display = "block";
+    
+ 
+    let users = JSON.parse(localStorage.getItem("users")) || []
+    let foundUser = users.find(u => u.email === email)
+    
+    if (foundUser) {
+        alert(`Um email de recuperação de senha foi enviado para ${email}`)
+        window.location.href = "index.html";
+    } else {
+        document.getElementById("erroLogin").style.display = "block"
         document.getElementById("email").value = ""
     }
-
 }
